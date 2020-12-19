edba82d9afd3cf818aaed4ef60acaddd2f0e114c
JPark <0814owl@gmail.com>
2020-12-19 09:07:52 +0900

    1219 update
    base

---
+++ Assets/scripts/UIWork.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIWork : MonoBehaviour
+{
+    public buttonManager BMscript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BMscript = GameObject.Find("EventSystem").GetComponent<buttonManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Reset_Numbers()
+    {
+        for(int i = 0; i < 45; ++i)
+        {
+            BMscript.buttonNumbers[i].GetComponent<buttonStat>().ChangeToNope();
+        }
+        BMscript.current_button_number = 0;
+    }
+}
---
+++ Assets/scripts/buttonManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonManager : MonoBehaviour
+{
+    public GameObject[] buttonNumbers = new GameObject[45];
+    public int max_button_number = 6;
+    public int current_button_number;
+    // Start is called before the first frame update
+    void Start()
+    {
+        max_button_number = 6;
+        current_button_number = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
